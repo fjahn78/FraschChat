72f2235d90d5d0816009a01257ff992fd6175b4b
Frank Schumann <Frank.Schumann@prosozial.de>
2016-08-26 11:54:10 +0200

    add functionality to type and display text

--- src/com/frasch/fraschchat/client/ClientGUI.java
+++ src/com/frasch/fraschchat/client/ClientGUI.java
@@ -13,17 +13,18 @@ import javax.swing.JButton;
 import javax.swing.JTextField;
 import javax.swing.border.EtchedBorder;
 import javax.swing.JSeparator;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class ClientGUI extends JFrame {
 
 	private static final long serialVersionUID = 1L;
 	private JPanel contentPane;
 	
-	/** The m name. */
 	private String name, address;
 	private int port;
+	private JTextArea textArea = new JTextArea();
 	private JTextField txtMessage;
-	private JSeparator separator;
 
 	/**
 	 * Create the frame.
@@ -37,6 +38,7 @@ public class ClientGUI extends JFrame {
 		this.port = port;
 		
 		createWindow();
+		console("Successfully connected to " + this.address + ":" + this.port + "; user: " + this.name);
 	}
 
 	/**
@@ -58,12 +60,12 @@ public class ClientGUI extends JFrame {
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
 		gbl_contentPane.columnWidths = new int[]{0};
-		gbl_contentPane.rowHeights = new int[]{0, 0};
+		gbl_contentPane.rowHeights = new int[]{0};
 		gbl_contentPane.columnWeights = new double[]{Double.MIN_VALUE};
-		gbl_contentPane.rowWeights = new double[]{Double.MIN_VALUE, 0.0};
+		gbl_contentPane.rowWeights = new double[]{Double.MIN_VALUE};
 		contentPane.setLayout(gbl_contentPane);
 		
-		JTextArea textArea = new JTextArea();
+		textArea.setEditable(false);
 		GridBagConstraints gbc_textArea = new GridBagConstraints();
 		gbc_textArea.gridwidth = 2;
 		gbc_textArea.insets = new Insets(0, 0, 5, 0);
@@ -72,29 +74,48 @@ public class ClientGUI extends JFrame {
 		gbc_textArea.gridy = 0;
 		contentPane.add(textArea, gbc_textArea);
 		
-		separator = new JSeparator();
-		GridBagConstraints gbc_separator = new GridBagConstraints();
-		gbc_separator.gridwidth = 2;
-		gbc_separator.insets = new Insets(0, 0, 5, 5);
-		gbc_separator.gridx = 0;
-		gbc_separator.gridy = 1;
-		contentPane.add(separator, gbc_separator);
-		
 		txtMessage = new JTextField();
 		GridBagConstraints gbc_txtMessage = new GridBagConstraints();
 		gbc_txtMessage.insets = new Insets(0, 0, 0, 5);
 		gbc_txtMessage.fill = GridBagConstraints.HORIZONTAL;
 		gbc_txtMessage.gridx = 0;
-		gbc_txtMessage.gridy = 2;
+		gbc_txtMessage.gridy = 1;
 		contentPane.add(txtMessage, gbc_txtMessage);
 		txtMessage.setColumns(10);
 		
 		JButton btnSend = new JButton("Send");
+		btnSend.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				doEcho();
+			}
+
+		});
 		GridBagConstraints gbc_btnSend = new GridBagConstraints();
 		gbc_btnSend.gridx = 1;
-		gbc_btnSend.gridy = 2;
+		gbc_btnSend.gridy = 1;
 		contentPane.add(btnSend, gbc_btnSend);
+		
 		this.setVisible(true);
+		txtMessage.requestFocusInWindow();
 	}
 
+	/**
+	 * print the message into the Console text area.
+	 *
+	 * @param message the message
+	 */
+	public void console(String message) {
+		textArea.append(message+"\n\r");
+	}
+
+	/**
+	 * Send the input to the text area and clear the input field.
+	 */
+	private void doEcho() {
+		String input = txtMessage.getText();
+		if (!input.equals("")) 
+			console(name + ": " + input);
+		txtMessage.setText("");
+		txtMessage.requestFocusInWindow();
+	}
 }
