ab5c7c7800585313a22d8d40d68d96f83eaf809f
Frank Schumann <Frank.Schumann@prosozial.de>
2016-08-26 10:47:05 +0200

    Add GUI elements

--- src/com/frasch/fraschchat/client/ClientGUI.java
+++ src/com/frasch/fraschchat/client/ClientGUI.java
@@ -1,10 +1,18 @@
 package com.frasch.fraschchat.client;
 
+import java.awt.Color;
 import java.awt.GridBagLayout;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.UIManager;
+import javax.swing.JTextArea;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import javax.swing.JButton;
+import javax.swing.JTextField;
+import javax.swing.border.EtchedBorder;
+import javax.swing.JSeparator;
 
 public class ClientGUI extends JFrame {
 
@@ -14,6 +22,8 @@ public class ClientGUI extends JFrame {
 	/** The m name. */
 	private String name, address;
 	private int port;
+	private JTextField txtMessage;
+	private JSeparator separator;
 
 	/**
 	 * Create the frame.
@@ -33,17 +43,57 @@ public class ClientGUI extends JFrame {
 	 * Creates the chat window
 	 */
 	private void createWindow() {
+		try {
+			UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		setTitle("FraschChat");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setSize(800, 600);
 		setLocationRelativeTo(null);
 		
 		contentPane = new JPanel();
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setBackground(Color.LIGHT_GRAY);
 		setContentPane(contentPane);
 		GridBagLayout gbl_contentPane = new GridBagLayout();
-		gbl_contentPane.columnWeights = new double[]{};
-		gbl_contentPane.rowWeights = new double[]{};
+		gbl_contentPane.columnWidths = new int[]{0};
+		gbl_contentPane.rowHeights = new int[]{0, 0};
+		gbl_contentPane.columnWeights = new double[]{Double.MIN_VALUE};
+		gbl_contentPane.rowWeights = new double[]{Double.MIN_VALUE, 0.0};
 		contentPane.setLayout(gbl_contentPane);
+		
+		JTextArea textArea = new JTextArea();
+		GridBagConstraints gbc_textArea = new GridBagConstraints();
+		gbc_textArea.gridwidth = 2;
+		gbc_textArea.insets = new Insets(0, 0, 5, 0);
+		gbc_textArea.fill = GridBagConstraints.BOTH;
+		gbc_textArea.gridx = 0;
+		gbc_textArea.gridy = 0;
+		contentPane.add(textArea, gbc_textArea);
+		
+		separator = new JSeparator();
+		GridBagConstraints gbc_separator = new GridBagConstraints();
+		gbc_separator.gridwidth = 2;
+		gbc_separator.insets = new Insets(0, 0, 5, 5);
+		gbc_separator.gridx = 0;
+		gbc_separator.gridy = 1;
+		contentPane.add(separator, gbc_separator);
+		
+		txtMessage = new JTextField();
+		GridBagConstraints gbc_txtMessage = new GridBagConstraints();
+		gbc_txtMessage.insets = new Insets(0, 0, 0, 5);
+		gbc_txtMessage.fill = GridBagConstraints.HORIZONTAL;
+		gbc_txtMessage.gridx = 0;
+		gbc_txtMessage.gridy = 2;
+		contentPane.add(txtMessage, gbc_txtMessage);
+		txtMessage.setColumns(10);
+		
+		JButton btnSend = new JButton("Send");
+		GridBagConstraints gbc_btnSend = new GridBagConstraints();
+		gbc_btnSend.gridx = 1;
+		gbc_btnSend.gridy = 2;
+		contentPane.add(btnSend, gbc_btnSend);
 		this.setVisible(true);
 	}
 
