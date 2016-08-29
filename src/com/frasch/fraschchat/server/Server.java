1760453cf686e29b2d3029e43340d0e05c0d8905
Frank Schumann <Frank.Schumann@prosozial.de>
2016-08-29 14:20:45 +0200

    Add new package com.frasch.fraschchat.server w/ server and servermain

---
+++ src/com/frasch/fraschchat/server/Server.java
@@ -0,0 +1,26 @@
+/**
+ * The class Server manages all client connections and chat transmission
+ * 
+ */
+package com.frasch.fraschchat.server;
+
+public class Server implements Runnable {
+
+	private int port;
+	
+	
+	/**
+	 * @param port
+	 */
+	public Server(int port) {
+		this.port = port;
+	}
+
+
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ src/com/frasch/fraschchat/server/ServerMain.java
@@ -0,0 +1,30 @@
+/**
+ * The ServerMain class serves as command line front end
+ * 
+ */
+package com.frasch.fraschchat.server;
+
+/**
+ * @author FraSch
+ *
+ */
+public class ServerMain implements Runnable {
+
+	/* (non-Javadoc)
+	 * @see java.lang.Runnable#run()
+	 */
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
